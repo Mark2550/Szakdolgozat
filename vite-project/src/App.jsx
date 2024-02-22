50fd544bc4087e31662121de91b2873e86d179ef
Kárpáti Márk János <Sakkmester4@gmail.com>
2024-02-22 16:45:51 +0100

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+///import Navbar from './components/Navbar'
+import SignIn from './components/SignIn'
+
+const App = () => {
+
+  return (
+    <>
+      <div className="card">
+          <SignIn />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <nav className='bg-white'>
+        <div className='bg-white'>
+            <ul className=' '>
+                <li className=' '>
+                    <a href="">ButtonAkármi legalább műkszik</a>
+                </li>
+            </ul>
+        </div>
+
+
+    </nav>
+  )
+}
+
+export default Navbar
