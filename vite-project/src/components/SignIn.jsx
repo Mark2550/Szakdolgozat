9f7fb2d51566399794be4c6fc047326a8c2524f1
Márk <Sakkmester4@gmail.com>
2024-02-24 19:16:58 +0100

    a

--- vite-project/src/components/SignIn.jsx
+++ vite-project/src/components/SignIn.jsx
@@ -17,7 +17,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Zenekereső
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
