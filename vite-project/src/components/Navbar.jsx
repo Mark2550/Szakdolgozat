3e8e331ae13185cf0c282079bc0df60d77a2a73f
Kárpáti Márk János <Sakkmester4@gmail.com>
2024-03-15 22:02:54 +0100

    No changes in navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <nav className='bg-white'>
-        <div className='bg-white'>
+    <nav>
+        <div className=' '>
             <ul className=' '>
                 <li className=' '>
                     <a href="">ButtonAkármi legalább műkszik</a>
                 </li>
             </ul>
         </div>
-
-
     </nav>
   )
 }
