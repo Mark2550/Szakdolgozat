3ba60ce3883c6cced06295011093890d6c088cd8
Márk <Sakkmester4@gmail.com>
2024-04-07 23:09:27 +0200

    Fine

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,17 +10,17 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const App = () => {
   return (
-    <BrowserRouter> 
-    {/** For parent element only */}
-      <Navbar />
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/LogIn" element={<LogIn />} />
-          <Route path="*" element={<NoPage />} />
-          {/** The '*' stands for all type of links */}
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter> 
+      {/** For parent element only */}
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/LogIn" element={<LogIn />} />
+            <Route path="*" element={<NoPage />} />
+            {/** The '*' stands for all type of links */}
+        </Routes>
+      </BrowserRouter>
   )
 }
 
