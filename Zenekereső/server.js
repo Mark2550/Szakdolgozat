a403f450017ce63730f379fe7729c924c9d6d6c4
Kárpáti Márk János <Sakkmester4@gmail.com>
2024-04-15 22:17:43 +0200

    Import&file improvements

---
+++ Zenekereső/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5173 ;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
