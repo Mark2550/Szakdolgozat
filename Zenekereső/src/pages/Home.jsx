86e49868c859c40560a443068aa5213294d927a1
Márk <Sakkmester4@gmail.com>
2024-04-24 18:06:29 +0200

    Minor improvements

--- Zenekereső/src/pages/Home.jsx
+++ Zenekereső/src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import Fileupload from "../components/Fileupload"
 import Searchbar from "../components/Searchbar"
-
+import Table from "../components/Table"
 
 const Mainpage = () => {
     return (
-        <div>
-            <Searchbar />
+        <div className="">
+            <div className="">
+                <Searchbar />
+                <br />
+                <Table />
+            </div>
+            <div className="overflow-scroll">
+                
+            </div>
             <div>
                 <h1 className="text-3xl font-bold">
                     Upload file here
                 </h1>
-                <br />
             </div>
             <Fileupload/>
         </div>
